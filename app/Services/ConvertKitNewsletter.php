2cd5f2efa6016fcda7b232b0299e93f3cc1cae20
Spectrum842 <mauranyapin.nathan@gmail.com>
2022-05-13 12:32:51 +0200

    Chapter-11 Terminado

---
+++ app/Services/ConvertKitNewsletter.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Services;
+
+
+class ConvertKitNewsletter{
+
+    public function subscribe(string $email, string $list = null){
+
+        $list ??= config('services.mailchimp.lists.subscribers');
+
+        return $this->client->lists->addListMember($list,[
+            'email_address' => $email,
+            'status' => 'subscribed',
+        ]);
+
+
+    }
+}
